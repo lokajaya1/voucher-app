30b35431698754d0acf31f65fa210c230730590c
Lokajaya Andala <lokajayandala1@gmail.com>
2024-11-25 02:54:54 +0700

    modified navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Navbar from "@/components/navbar/Navbar";
 
 export const metadata = {
   title: "Voucher App",
@@ -12,22 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <main className="bg-white text-black">
-      {/* Navbar */}
-      <header className="w-full bg-black text-white px-4 py-4">
-        <div className="max-w-5xl mx-auto flex items-center justify-between">
-          <h1 className="text-2xl font-bold">LokVoucher</h1>
-          <nav className="space-x-4">
-            <Link href="/voucher" className="text-gray-300 hover:text-white">
-              Vouchers
-            </Link>
-            <Link href="/history" className="text-gray-300 hover:text-white">
-              History
-            </Link>
-          </nav>
-        </div>
-      </header>
-
-      {/* Layout Body */}
+      <Navbar />
       <div className="flex">
         {/* Main Content */}
         <section className="flex-1 px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
